fb88aca0f78bcf0345866be98eb2b51df1810293
Luis Felipe <lipebezerraa@gmail.com>
2021-04-11 16:48:34 -0300

    Primeiro Comimit

---
+++ App/Connection.php
@@ -0,0 +1,23 @@
+<?php
+ namespace App;
+
+
+
+class Connection{
+     public static function getDb(){
+         try{
+
+            $conn = new \PDO(
+                "mysql:host=localhost; dbname=mvc",
+                "root",
+                ""
+            );
+            return $conn;
+
+         }catch( \PDOException $e){
+            echo "ERROR";
+         }
+     }
+ }
+
+?>
